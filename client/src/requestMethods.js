7d60a5dbe6b36ba77a4605b677b2940776304c98
venkatesh <venkateshvnky3803@gmail.com>
2024-03-07 14:58:03 +0530

    updated backend url

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-export const BASE_URL = "http://localhost:5000/api"
+// export const BASE_URL = "http://localhost:5000/api"
+export const BASE_URL = "https://dns-managment.onrender.com/api"
 
 // ---------------------------Dns records requests
 
